c3d35865ccae9e415f80b3c8d62dd77c2b508e72
dorokhov.vladimir@gmail.com <dorokhov.vladimir@gmail.com>
2016-04-24 11:58:02 +0300

    removed temp folders

--- built/local/app.module.js
+++
@@ -1,9 +0,0 @@
-/// <reference path='../_all.ts' />
-var app;
-(function (app) {
-    'use str1ict';
-    angular.module('app', []);
-})(app || (app = {}));
-/// <reference path='../typings/jquery/jquery.d.ts' />
-/// <reference path='../typings/angularjs/angular.d.ts' />
-/// <reference path='app/app.module.ts' /> 
--- dist/startup.js
+++
@@ -1,8 +0,0 @@
-﻿chrome.app.runtime.onLaunched.addListener(function () {
-    chrome.app.window.create('index.html', {
-        'bounds': {
-            'width': 1170,
-            'height': 800
-        }
-    });
-});
