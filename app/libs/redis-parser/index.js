92fc3c843dd7ec170ba58be08bc5cbda46b85caf
dorokhov.vladimir@gmail.com <dorokhov.vladimir@gmail.com>
2017-05-13 22:16:06 +0300

    added node redis

---
+++ app/libs/redis-parser/index.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = require('./lib/parser.js')
+module.exports.ReplyError = require('./lib/replyError.js')
+module.exports.RedisError = require('./lib/redisError.js')
+module.exports.ParserError = require('./lib/redisError.js')
